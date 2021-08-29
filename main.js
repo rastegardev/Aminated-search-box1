f0ef82911f7dcd7aa87f17f9245cd72bfd483491
reza rastegar <85369490+rezarastegar2003@users.noreply.github.com>
2021-08-29 15:18:40 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const searchBtn = document.querySelector(".search-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const searchBox = document.querySelector(".search-box");
+
+searchBtn.onclick = () => {
+  searchBox.classList.add("active");
+}
+
+cancelBtn.onclick = () => {
+  searchBox.classList.remove("active");
+}
